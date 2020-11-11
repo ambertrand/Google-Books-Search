892197caff8d89fb6b1c8836a28a65e7a1264429
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-11-10 21:40:21 -0500

    starting saved and search pages in react

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from 'react-bootstrap';
+import Row from 'react-bootstrap';
+
+const Saved = () => {
+    return(
+        <Container>
+            <Row>
+                <h1>Saved Data</h1>
+            </Row>
+        </Container>
+    )
+}
+
+export default Saved;
---
+++ client/src/pages/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Container from 'react-bootstrap';
+import Row from 'react-bootstrap';
+import Col from 'react-bootstrap';
+
+const Search = () => {
+    return(
+        <Container>
+            <Row>
+                <Col>
+                    <h1>Search Bar</h1>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Search;
