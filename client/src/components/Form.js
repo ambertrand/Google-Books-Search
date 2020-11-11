4e011f1992dbccf470ea30b5ab766eb73a30eae1
Alex Bertrand <alex.m.bertrand@gmail.com>
2020-11-10 22:24:02 -0500

    Added book search form

---
+++ client/src/components/Form.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export function SearchBox(props) {
+    return (
+        <div className="form-group">
+            <input type="search" className="form-control" {...props} />
+        </div>
+    );
+}
+
+
+export function FormBtn(props) {
+    return (
+        <button {...props} style={{ float: "left", marginBottom: 10 }} className="btn btn-primary">
+            {props.children}
+        </button>
+    );
+}
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -2,13 +2,21 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { SearchBox, FormBtn } from '../components/Form';
 
 const Search = () => {
     return(
         <Container>
             <Row>
                 <Col>
-                    <h1>Search Bar</h1>
+                    <form>
+                        <p>Book Search</p>
+                        <SearchBox
+                            name="book search"
+                            placeholder="Book Title"
+                        />
+                        <FormBtn>Search</FormBtn>
+                    </form>
                 </Col>
             </Row>
         </Container>
